d13297145fa5dc3682e023412d410c5aceb1596a
강단 <kkangdanni@sk.com>
2024-04-15 16:44:29 +0900

    pdf출력기능

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
   path('<int:question_id>/', views.detail, name='detail'),
   path('<int:question_id>/results/', views.results, name='results'),
   path('<int:question_id>/vote/', views.vote, name='vote'),
-
+  path('download/', views.render_pdf_view, name='download'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render,get_object_or_404
 # Create your views here.
-from django.http import HttpResponse,  HttpResponseRedirect
+from django.http import HttpResponse,  HttpResponseRedirect,FileResponse
 from django.template import loader
 from django.urls import reverse
 from .models import Question, Choice
+
+import os
+from django.conf import settings
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+from django.contrib.staticfiles import finders
 
 def index(request):
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -41,3 +47,52 @@ def vote(request, question_id):
       # with POST data. This prevents data from being posted twice if a
       # user hits the Back button.
       return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+  
+def link_callback(uri, rel):
+    """
+    Convert HTML URIs to absolute system paths so xhtml2pdf can access those
+    resources
+    """
+    result = finders.find(uri)
+    if result:
+            if not isinstance(result, (list, tuple)):
+                    result = [result]
+            result = list(os.path.realpath(path) for path in result)
+            path=result[0]
+    else:
+            sUrl = settings.STATIC_URL        # Typically /static/
+            sRoot = settings.STATIC_ROOT      # Typically /home/userX/project_static/
+            mUrl = settings.MEDIA_URL         # Typically /media/
+            mRoot = settings.MEDIA_ROOT       # Typically /home/userX/project_static/media/
+
+            if uri.startswith(mUrl):
+                    path = os.path.join(mRoot, uri.replace(mUrl, ""))
+            elif uri.startswith(sUrl):
+                    path = os.path.join(sRoot, uri.replace(sUrl, ""))
+            else:
+                    return uri
+
+    # make sure that file exists
+    if not os.path.isfile(path):
+            raise RuntimeError(
+                    'media URI must start with %s or %s' % (sUrl, mUrl)
+            )
+    return path
+    
+def render_pdf_view(request):
+    template_path = 'polls/generate_pdf.html'
+    context = {'myvar': 'this is your template context'}
+    # Create a Django response object, and specify content_type as pdf
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+    # find the template and render it.
+    template = get_template(template_path)
+    html = template.render(context)
+
+    # create a pdf
+    pisa_status = pisa.CreatePDF(
+       html, dest=response, link_callback=link_callback)
+    # if error then show some funny view
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
